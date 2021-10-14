2022cce4cef4288ba295b113f5edcef169d8a4de
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-10-13 22:25:52 -0300

    db-update para corrigir os status das inscrições migradas para oportunidades

--- themes/CulturaZ/db-updates.php
+++ themes/CulturaZ/db-updates.php
@@ -1,6 +1,13 @@
 <?php
+
+use function MapasCulturais\__exec;
+
 $app = MapasCulturais\App::i();
 $em = $app->em;
 $conn = $em->getConnection();
 
-return array();
+return [
+    'fix-registrations' => function() {
+        __exec("UPDATE registration SET number = concat('on-', id), consolidated_result = status");
+    }
+];
