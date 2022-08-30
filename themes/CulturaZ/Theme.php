1aeab21953082f7679fd6bef62044edc8b61485e
santoandreculturadev <santoandrecultura@gmail.com>
2022-08-30 08:36:36 -0300

    escrita atualização

--- themes/CulturaZ/Theme.php
+++ themes/CulturaZ/Theme.php
@@ -10,11 +10,11 @@ class Theme extends BaseV1\Theme{
             'site: in the region' => 'na cidade de Santo André',
             'site: of the region' => 'da cidade de Santo André',
             'site: owner' => 'Secretaria da Cultura de Santo André',
-            'site: by the site owner' => 'pela Secretaria de Cultura e Turismo de Santo André',
+            'site: by the site owner' => 'pela Secretaria de Cultura de Santo André',
 
             'home: abbreviation' => "SCT",
 //            'home: colabore' => "Colabore com o Mapas Culturais",
-            'home: welcome' => "CulturAZ é a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria de Cultura e Turismo de Santo André sobre o cenário cultural andreense. Mais que a facilidade de se programar, o mapeamento permite conhecer a diversidade cultural que a cidade oferece e contribui na elaboração de políticas públicas. Além de conferir a agenda de shows musicais, espetáculos teatrais, sessões de cinema, saraus, entre outras, você também pode colaborar: basta criar perfil de agente cultural. A partir deste cadastro, fica mais fácil participar dos editais do Fundo de Cultura e também divulgar eventos, espaços ou projetos.",
+            'home: welcome' => "CulturAZ é a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria de Cultura de Santo André sobre o cenário cultural andreense. Mais que a facilidade de se programar, o mapeamento permite conhecer a diversidade cultural que a cidade oferece e contribui na elaboração de políticas públicas. Além de conferir a agenda de shows musicais, espetáculos teatrais, sessões de cinema, saraus, entre outras, você também pode colaborar: basta criar perfil de agente cultural. A partir deste cadastro, fica mais fácil participar dos editais do Fundo de Cultura e também divulgar eventos, espaços ou projetos.",
             'home: events' => "Você pode pesquisar eventos culturais da cidade nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
             'home: agents' => "Você pode colaborar na gestão da cultura da cidade com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural andreense. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
             'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais na cidade.",
