2358d579296fbe5f846dc71967465ca9f3d77d99
santoandreculturadev <santoandrecultura@gmail.com>
2023-01-31 18:13:00 -0300

    horario

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -3,6 +3,8 @@ use MapasCulturais\i;
 
 $_SERVER['SERVER_NAME'] = 'culturaz.santoandre.sp.gov.br';
 
+date_default_timezone_set('America/Sao_Paulo');
+
 return [
     'app.siteName' => 'CulturAZ Santo André',
     'app.siteDescription' => 'CulturaZ Santo André',
